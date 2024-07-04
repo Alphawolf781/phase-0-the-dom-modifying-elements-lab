ac8f19fadcd1f655145f87e1963cf5d7f3b58fac
Alphawolf781 <vincent.wambua@student.moringaschool.com>
2024-07-04 20:56:40 +0300

    completed the lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here
+const mainElement = document.querySelector('main#main')
+
+mainElement.remove()
+
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.textContent= 'vincent is the champion'
